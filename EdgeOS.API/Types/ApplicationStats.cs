5f727bbd8ae6a4da8b9d58754cdef0027805163d
Matthew <matthew1471@users.noreply.github.com>
2020-06-10 01:52:41 +0100

    Update documentation

--- EdgeOS.API/Types/ApplicationStats.cs
+++ EdgeOS.API/Types/ApplicationStats.cs
@@ -5,19 +5,19 @@ namespace EdgeOS.API.Types
     /// <summary>An object that contains traffic statistics for a certain observed application type via Deep Packet Inspection (DPI).</summary>
     public class ApplicationStats
     {
-        /// <summary>The application's transmitted bytes.</summary>
+        /// <summary>The number of bytes transmitted from this observed application.</summary>
         [JsonProperty(PropertyName = "tx_bytes")]
         uint tx_bytes;
 
-        /// <summary>The application's current transmit rate.</summary>
+        /// <summary>The rate at which bytes are currently being transmitted from this observed application.</summary>
         [JsonProperty(PropertyName = "tx_rate")]
         uint tx_rate;
 
-        /// <summary>The application's received bytes.</summary>
+        /// <summary>The number of bytes received by this observed application.</summary>
         [JsonProperty(PropertyName = "rx_bytes")]
         uint rx_bytes;
 
-        /// <summary>The application's current receive rate.</summary>
+        /// <summary>The rate at which bytes are currently being received by this observed application.</summary>
         [JsonProperty(PropertyName = "rx_rate")]
         uint rx_rate;
     }
